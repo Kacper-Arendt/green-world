4b8052f566d41814b7577891448d180de435b41e
kacper <kacykvaa@gmail.com>
2021-08-10 09:39:09 +0200

    feat add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import {createGlobalStyle} from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  *,
+  *::after,
+  *::before {
+    box-sizing: inherit;
+    margin: 0;
+    padding: 0;
+  }
+
+  html {
+    font-size: 62.5%;
+    width: 100%;
+    height: 100%;
+    background: var(--color-background);
+  }
+
+  :root {
+    --color-text: black;
+    --color-background: #EFEFEF;
+    --color-primary: green;
+  }
+`
 
 function App() {
-  return (
-    <div>
-     <h1>hello</h1>
-    </div>
-  );
+    return (
+        <div>
+            <GlobalStyles/>
+        </div>
+    );
 }
 
 export default App;
