3dbd65706e59d28312325eb30c8a81c273ddba08
kacper <kacykvaa@gmail.com>
2021-08-12 20:28:10 +0200

    feat add products container

--- src/components/Market/Market.tsx
+++ src/components/Market/Market.tsx
@@ -1,7 +1,38 @@
+import styled from "styled-components";
+import {ProductsData} from "../Products/ProductsData";
+import {Product} from "../Products/Product";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+
+  h2 {
+    font-size: 2.5rem;
+    width: 100%;
+    padding: 2rem;
+    background-color: #fff;
+  }
+`
+
 export const Market = () => {
     return (
-        <>
-
-        </>
+        <Wrapper>
+            <h2>Market</h2>
+            {ProductsData.map(el => {
+                return (
+                    <Product
+                        id={el.id}
+                        name={el.name}
+                        img={el.img}
+                        alt={el.alt}
+                        price={el.price}
+                        per={el.per}
+                        newPrice={el.newPrice}
+                    />
+                )
+            })}
+        </Wrapper>
     )
 }
